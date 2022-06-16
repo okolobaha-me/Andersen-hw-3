116cb3f3143dbaf0980b86f3b87f69788f7e704b
okolobaha-me <yevheniy.onyshchenko@gmail.com>
2022-06-16 11:16:57 +0300

    refactor Form component to function with hooks

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import s from './Form.module.css';
 import * as formValidation from '../../utils/formValidation';
 import { TextArea } from './TextArea/TextArea';
@@ -8,43 +8,72 @@ import { VALIDATION_MESSAGES } from '../../utils/formValidation';
 import { Input } from './Input/Input';
 import { maskPhone } from '../../utils/phoneMask';
 
-const { NOT_ENTERED, EMPTY } = VALIDATION_MESSAGES;
+export const Form = ({ onSubmit }) => {
+  const { NOT_ENTERED, EMPTY } = VALIDATION_MESSAGES;
 
-export class Form extends Component {
-  state = {
-    values: {
-      name: '',
-      lastName: '',
-      birthDate: '',
-      phone: '',
-      website: 'https://',
-      about: '',
-      tech: '',
-      project: '',
-    },
-    validStatus: {
-      name: VALIDATION_MESSAGES.NOT_ENTERED,
-      lastName: VALIDATION_MESSAGES.NOT_ENTERED,
-      birthDate: VALIDATION_MESSAGES.NOT_ENTERED,
-      phone: VALIDATION_MESSAGES.NOT_ENTERED,
-      website: VALIDATION_MESSAGES.NOT_ENTERED,
-      about: VALIDATION_MESSAGES.NOT_ENTERED,
-      tech: VALIDATION_MESSAGES.NOT_ENTERED,
-      project: VALIDATION_MESSAGES.NOT_ENTERED,
-    },
+  const [values, setValues] = useState({
+    name: '',
+    lastName: '',
+    birthDate: '',
+    phone: '',
+    website: 'https://',
+    about: '',
+    tech: '',
+    project: '',
+  });
+  const [validStatus, setValidStatus] = useState({
+    name: NOT_ENTERED,
+    lastName: NOT_ENTERED,
+    birthDate: NOT_ENTERED,
+    phone: NOT_ENTERED,
+    website: NOT_ENTERED,
+    about: NOT_ENTERED,
+    tech: NOT_ENTERED,
+    project: NOT_ENTERED,
+  });
+
+  const setFieldValue = (fieldName, value) => {
+    setValues(prevState => ({ ...prevState, [fieldName]: value }));
   };
 
-  setFieldValue = (fieldName, value) => {
-    this.setState(prev => ({ values: { ...prev.values, [fieldName]: value } }));
+  const setIsValidField = (fieldName, value) => {
+    setValidStatus(prevState => ({ ...prevState, [fieldName]: value }));
   };
 
-  setIsValidField = (fieldName, value) => {
-    this.setState(prev => ({
-      validStatus: { ...prev.validStatus, [fieldName]: value },
-    }));
+  const validate = e => {
+    const fieldName = e.currentTarget.name;
+    const value = e.currentTarget.value.trim();
+
+    switch (fieldName) {
+      case 'name':
+      case 'lastName':
+        setIsValidField(fieldName, formValidation.name(value));
+        break;
+
+      case 'phone':
+        setIsValidField(fieldName, formValidation.phone(value));
+        break;
+
+      case 'birthDate':
+        setIsValidField(fieldName, formValidation.date(value));
+        break;
+
+      case 'website':
+        setIsValidField(fieldName, formValidation.webSite(value));
+        break;
+
+      case 'about':
+      case 'tech':
+      case 'project':
+        setIsValidField(fieldName, formValidation.textArea(value));
+        break;
+
+      default:
+        break;
+    }
   };
 
-  handleChange = e => {
+  const handleChange = e => {
     const fieldName = e.currentTarget.name;
     let value = e.currentTarget.value;
 
@@ -61,75 +90,40 @@ export class Form extends Component {
         return;
     }
 
-    this.setFieldValue(fieldName, value);
+    setFieldValue(fieldName, value);
 
-    this.validate(e);
+    validate(e);
   };
 
-  validate = e => {
-    const fieldName = e.currentTarget.name;
-    const value = e.currentTarget.value.trim();
-
-    switch (fieldName) {
-      case 'name':
-      case 'lastName':
-        this.setIsValidField(fieldName, formValidation.name(value));
-        break;
-
-      case 'phone':
-        this.setIsValidField(fieldName, formValidation.phone(value));
-        break;
-
-      case 'birthDate':
-        this.setIsValidField(fieldName, formValidation.date(value));
-        break;
-
-      case 'website':
-        this.setIsValidField(fieldName, formValidation.webSite(value));
-        break;
-
-      case 'about':
-      case 'tech':
-      case 'project':
-        this.setIsValidField(fieldName, formValidation.textArea(value));
-        break;
-
-      default:
-        break;
-    }
-  };
-
-  clearForm = () => {
-    this.setState({
-      values: {
-        name: '',
-        lastName: '',
-        birthDate: '',
-        phone: '',
-        website: 'https://',
-        about: '',
-        tech: '',
-        project: '',
-      },
-      validStatus: {
-        name: 'notEntered',
-        lastName: 'notEntered',
-        birthDate: 'notEntered',
-        phone: 'notEntered',
-        website: 'notEntered',
-        about: 'notEntered',
-        tech: 'notEntered',
-        project: 'notEntered',
-      },
+  const clearForm = () => {
+    setValues({
+      name: '',
+      lastName: '',
+      birthDate: '',
+      phone: '',
+      website: 'https://',
+      about: '',
+      tech: '',
+      project: '',
+    });
+    setValidStatus({
+      name: 'notEntered',
+      lastName: 'notEntered',
+      birthDate: 'notEntered',
+      phone: 'notEntered',
+      website: 'notEntered',
+      about: 'notEntered',
+      tech: 'notEntered',
+      project: 'notEntered',
     });
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    if (!formValidation.isValidForm(Object.values(this.state.validStatus))) {
-      this.setState(prev => {
-        const {
+    if (!formValidation.isValidForm(Object.values(validStatus))) {
+      setValidStatus(
+        ({
           name,
           lastName,
           birthDate,
@@ -138,10 +132,8 @@ export class Form extends Component {
           about,
           tech,
           project,
-        } = prev.validStatus;
-
-        return {
-          validStatus: {
+        }) => {
+          return {
             name: name === NOT_ENTERED ? EMPTY : name,
             lastName: lastName === NOT_ENTERED ? EMPTY : lastName,
             birthDate: birthDate === NOT_ENTERED ? EMPTY : birthDate,
@@ -150,137 +142,128 @@ export class Form extends Component {
             about: about === NOT_ENTERED ? EMPTY : about,
             tech: tech === NOT_ENTERED ? EMPTY : tech,
             project: project === NOT_ENTERED ? EMPTY : project,
-          },
-        };
-      });
+          };
+        }
+      );
       return;
     }
 
-    this.props.onSubmit(this.state.values);
+    onSubmit(values);
   };
 
-  render() {
-    //Destructuring values
-    const { name, lastName, birthDate, phone, website, about, tech, project } =
-      this.state.values;
+  const { name, lastName, phone, website, project, tech, about, birthDate } =
+    values;
 
-    const isValid = this.state.validStatus;
+  return (
+    <>
+      <Title title={'Создание анкеты'} />
 
-    return (
-      <>
-        <Title title={'Создание анкеты'} />
-
-        <form className={s.form} onSubmit={this.handleSubmit}>
-          <div className={s.nameWrapper}>
-            {/*name input*/}
-            <Input
-              title={'Имя'}
-              fieldName={'name'}
-              type={'text'}
-              placeholder={'Введите имя'}
-              value={name}
-              onChange={this.handleChange}
-              validate={this.validate}
-              isValid={isValid.name}
-            />
-
-            {/*last name input*/}
-            <Input
-              title={'Фамилия'}
-              fieldName={'lastName'}
-              type={'text'}
-              placeholder={'Введите фамилию'}
-              value={lastName}
-              onChange={this.handleChange}
-              validate={this.validate}
-              isValid={isValid.lastName}
-            />
-          </div>
-
-          {/*birthdate input*/}
+      <form className={s.form} onSubmit={handleSubmit}>
+        <div className={s.nameWrapper}>
+          {/*name input*/}
           <Input
-            title={'Дата рождения'}
-            fieldName={'birthDate'}
-            type={'date'}
-            placeholder={'Ввыберите дату рождения'}
-            value={birthDate}
-            onChange={this.handleChange}
-            validate={this.validate}
-            isValid={isValid.birthDate}
+            title={'Имя'}
+            fieldName={'name'}
+            type={'text'}
+            placeholder={'Введите имя'}
+            value={name}
+            onChange={handleChange}
+            validate={validate}
+            isValid={validStatus.name}
           />
 
-          {/*phone input*/}
+          {/*last name input*/}
           <Input
-            title={'Телефон'}
-            fieldName={'phone'}
-            type={'tel'}
-            placeholder={'Укажите ваш телефон'}
-            value={maskPhone(phone)}
-            onChange={this.handleChange}
-            validate={this.validate}
-            isValid={isValid.phone}
+            title={'Фамилия'}
+            fieldName={'lastName'}
+            type={'text'}
+            placeholder={'Введите фамилию'}
+            value={lastName}
+            onChange={handleChange}
+            validate={validate}
+            isValid={validStatus.lastName}
           />
+        </div>
 
-          {/*website input*/}
-          <Input
-            title={'Сайт'}
-            fieldName={'website'}
-            type={'url'}
-            placeholder={'Укажите ваш сайт'}
-            value={website}
-            onChange={this.handleChange}
-            validate={this.validate}
-            isValid={isValid.website}
-          />
+        {/*birthdate input*/}
+        <Input
+          title={'Дата рождения'}
+          fieldName={'birthDate'}
+          type={'date'}
+          placeholder={'Ввыберите дату рождения'}
+          value={birthDate}
+          onChange={handleChange}
+          validate={validate}
+          isValid={validStatus.birthDate}
+        />
 
-          {/*about yourself input*/}
-          <TextArea
-            title={'О себе'}
-            onBlur={this.validate}
-            isValid={isValid.about}
-            onChange={this.handleChange}
-            name={'about'}
-            value={about}
-          />
+        {/*phone input*/}
+        <Input
+          title={'Телефон'}
+          fieldName={'phone'}
+          type={'tel'}
+          placeholder={'Укажите ваш телефон'}
+          value={maskPhone(phone)}
+          onChange={handleChange}
+          validate={validate}
+          isValid={validStatus.phone}
+        />
 
-          {/*technologies stack input*/}
-          <TextArea
-            title={'Стек технологий'}
-            onBlur={this.validate}
-            isValid={isValid.tech}
-            onChange={this.handleChange}
-            name={'tech'}
-            value={tech}
-          />
+        {/*website input*/}
+        <Input
+          title={'Сайт'}
+          fieldName={'website'}
+          type={'url'}
+          placeholder={'Укажите ваш сайт'}
+          value={website}
+          onChange={handleChange}
+          validate={validate}
+          isValid={validStatus.website}
+        />
 
-          {/*last project input*/}
-          <TextArea
-            title={'Описание последнего проекта'}
-            onBlur={this.validate}
-            isValid={isValid.project}
-            onChange={this.handleChange}
-            name={'project'}
-            value={project}
-          />
+        {/*about yourself input*/}
+        <TextArea
+          title={'О себе'}
+          onBlur={validate}
+          isValid={validStatus.about}
+          onChange={handleChange}
+          name={'about'}
+          value={about}
+        />
 
-          {/*form controls*/}
-          <div className={s.formControlWrapper}>
-            <button
-              type="button"
-              className={s.formControl}
-              onClick={this.clearForm}
-            >
-              Отмена
-            </button>
-            <button type="submit" className={s.formControl}>
-              Сохранить
-            </button>
-          </div>
-        </form>
-      </>
-    );
-  }
-}
+        {/*technologies stack input*/}
+        <TextArea
+          title={'Стек технологий'}
+          onBlur={validate}
+          isValid={validStatus.tech}
+          onChange={handleChange}
+          name={'tech'}
+          value={tech}
+        />
+
+        {/*last project input*/}
+        <TextArea
+          title={'Описание последнего проекта'}
+          onBlur={validate}
+          isValid={validStatus.project}
+          onChange={handleChange}
+          name={'project'}
+          value={project}
+        />
+
+        {/*form controls*/}
+        <div className={s.formControlWrapper}>
+          <button type="button" className={s.formControl} onClick={clearForm}>
+            Отмена
+          </button>
+          <button type="submit" className={s.formControl}>
+            Сохранить
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
 
 Form.propTypes = {
   onSubmit: PropTypes.func.isRequired,
