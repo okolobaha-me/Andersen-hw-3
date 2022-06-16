afc22d2bbf65d8414b7f713c9c9b7a784efc02a3
okolobaha-me <yevheniy.onyshchenko@gmail.com>
2022-06-16 11:24:17 +0300

    refactor ProfileField component to function with hooks

--- src/components/Profile/ProfileField/ProfileField.jsx
+++ src/components/Profile/ProfileField/ProfileField.jsx
@@ -1,18 +1,13 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import s from '../Profile.module.css';
 
-export class ProfileField extends Component {
-  render() {
-    const { fieldName, value } = this.props;
-
-    return (
-      <p className={s.text}>
-        <span className={s.fieldName}>{fieldName}: </span> {value}
-      </p>
-    );
-  }
-}
+export const ProfileField = ({ fieldName, value }) => {
+  return (
+    <p className={s.text}>
+      <span className={s.fieldName}>{fieldName}: </span> {value}
+    </p>
+  );
+};
 
 ProfileField.propTypes = {
   fieldName: PropTypes.string.isRequired,
